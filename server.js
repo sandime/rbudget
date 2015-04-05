2856dc2cd354e29432f7b1f6a7714c3b81493567
sherri <sherri@sherricruz.com>
2015-04-04 22:10:55 -0700

    changed server; added gitignore

--- server.js
+++ server.js
@@ -2,11 +2,27 @@
  * Created by SHERRI on 4/4/15.
  */
 //simple server
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var path = require('path');
+var port = 8000,
+    express = require('express'),
+    app = express();
+app.use('/', express.static(__dirname));
+app.listen(port);
+console.log('and the magic is here: http://localhost:'+port+'/index.html');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+
+/*
+
+ var express = require('express');
+ var app = express();
+
+ app.set('port', (process.env.PORT || 5000));
+ app.use(express.static(__dirname + '/public'));
+
+ app.get('/', function(request, response) {
+ response.send('Hello World!');
+ });
+
+ app.listen(app.get('port'), function() {
+ console.log("Node app is running at localhost:" + app.get('port'));
+ });
+*/
