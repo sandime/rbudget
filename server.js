66b294c63f51b87e56119bde4eee7179c52eda54
sherri <sherri@sherricruz.com>
2015-04-04 18:58:48 -0700

    changed server

--- server.js
+++ server.js
@@ -4,9 +4,14 @@
 //simple server
 var express = require('express');
 var app = express();
-var path = require ('path');
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+var port = process.env.PORT || 8080;
+var path = require('path');
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
-app.listen(8080);
-console.log('8080 is the magic port');
+
+app.listen(port)
+console.log('Magic happens on http://localhost:' + port);
