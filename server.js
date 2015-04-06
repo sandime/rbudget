5bac9ae6816b1c8e393c88c5a3a60254b15f4ae1
sherri <sherri@sherricruz.com>
2015-04-05 17:49:43 -0700

    changed server

--- server.js
+++ server.js
@@ -2,27 +2,17 @@
  * Created by SHERRI on 4/4/15.
  */
 //simple server
-var port = 8000,
-    express = require('express'),
-    app = express();
-app.use('/', express.static(__dirname));
-app.listen(port);
-console.log('and the magic is here: http://localhost:'+port+'/index.html');
+var express = require('express');
+var app = express();
+var path = require ('path');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/index'));
 
-
-/*
-
- var express = require('express');
- var app = express();
-
- app.set('port', (process.env.PORT || 5000));
- app.use(express.static(__dirname + '/public'));
-
- app.get('/', function(request, response) {
- response.send('Hello World!');
+ app.get('/', function (req, res) {
+ res.sendFile(path.join(__dirname + '/index.html'));
  });
 
- app.listen(app.get('port'), function() {
- console.log("Node app is running at localhost:" + app.get('port'));
- });
-*/
+app.listen(app.get('port'), function() {
+    console.log("magic is on port :" + app.get('port'));
+});
+
