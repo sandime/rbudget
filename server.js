9ad271f21cb499cf8cb335954461140f0f5bf2eb
sherri <sherri@sherricruz.com>
2015-04-07 11:42:27 -0700

    update before tweaks

--- server.js
+++ server.js
@@ -2,17 +2,27 @@
  * Created by SHERRI on 4/4/15.
  */
 //simple server
+    /*
+ var express = require('express');
+var app = express();
+var path = require ('path');
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.listen(1337);
+console.log("magic");
+*/
+
+
 var express = require('express');
 var app = express();
 var path = require ('path');
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/index'));
-
+app.set('port', (process.env.PORT || 8000));
  app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname + '/index.html'));
  });
-
 app.listen(app.get('port'), function() {
-    console.log("magic is on port :" + app.get('port'));
+    console.log("magic is on port:" + app.get('port'));
 });
+
 
